4e12ff3d011775f3a6efce8010a926220f2c5728
Andaya334 <Andaya334@gmail.com>
2023-05-04 20:20:26 -0700

    new save

--- logic.js
+++ logic.js
@@ -1,17 +1,18 @@
-// API endpoint
+// Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 var query2 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
 
-  // popup feature
+  // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
@@ -20,8 +21,8 @@ function createFeatures(earthquakeData) {
 
   
 
-  // Create GeoJSON layer 
-  // Run onEachFeature function
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
     pointToLayer: function (feature, latlng) {
@@ -44,32 +45,26 @@ function createFeatures(earthquakeData) {
   });
 
 
-  // Sending earthquake layer to the createMap function
+  // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
   
 }
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
 
-  var baseMaps = {
-    "Street Map": streetmap
-  };
-
+  // Create overlay object to hold our overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [streetmap, earthquakes]
+    layers: [earthquakes]
   });
 
 
@@ -86,7 +81,7 @@ function createMap(earthquakes) {
                         'rgb(255,0,0)';
   }
 
-  // Create a legend to display information about map
+  // Create a legend to display information about our map
   var legend = L.control({position: 'bottomright'});
 
   legend.onAdd = function (map) {
@@ -97,7 +92,7 @@ function createMap(earthquakes) {
 
       div.innerHTML+='Magnitude<br><hr>'
   
-      // loop through density intervals
+      // loop through our density intervals and generate a label with a colored square for each interval
       for (var i = 0; i < grades.length; i++) {
           div.innerHTML +=
               '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
